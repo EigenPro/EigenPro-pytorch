e2e2bf863e7868efe456076c373a9bf4eef37ba0
Parthe Pandit <parthepandit@ucla.edu>
2022-11-20 10:01:39 -0800

    modifed method  with new torch requirements

--- eigenpro.py
+++ eigenpro.py
@@ -97,8 +97,7 @@ class FKR_EigenPro(nn.Module):
             torch.cuda.empty_cache()
 
     def tensor(self, data, dtype=None, release=False):
-        tensor = torch.tensor(data, dtype=dtype,
-                              requires_grad=False).to(self.device)
+        tensor = torch.tensor(data, dtype=dtype, device=self.device)
         if release:
             self.pinned_list.append(tensor)
         return tensor
