0e1e64d229ba84b6651df6eeeedff79fc01418bb
Parthe Pandit <parthe@users.noreply.github.com>
2023-01-28 21:26:49 -0800

    Local imports

--- eigenpro/__init__.py
+++ eigenpro/__init__.py
@@ -1,3 +1,4 @@
-from .svd import nystrom_kernel_svd
-from .kernels import *
+# from .svd import nystrom_kernel_svd
+# from .kernels import *
 from .regressor import EigenProRegressor
+import .svd, .kernels
