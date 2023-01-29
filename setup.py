b034703165e22d6ec83a383372ae1add1a2fc707
Parthe Pandit <parthe1292@gmail.com>
2023-01-28 17:29:11 -0800

    changed name of package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='eigenpro',
     version='2.0.0',
     author='Siyuan Ma, Adityanarayanan Radhakrishnan, Parthe Pandit',
     author_email='parthe1292@gmail.com',
