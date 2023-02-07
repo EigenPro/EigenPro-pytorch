d145427d0717aacd4b676d32a5a187f4fd67e416
Parthe Pandit <parthe@users.noreply.github.com>
2023-02-06 22:14:25 -0800

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -1,7 +1,7 @@
 import torch
 import datasets
-import eigenpro
-from eigenpro.kernels import laplacian, ntk_relu_unit_sphere as relu_ntk
+import eigenpro2
+from eigenpro2.kernels import laplacian, ntk_relu_unit_sphere as relu_ntk
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,6 +13,6 @@ x_test=x_test/x_test.norm(dim=-1,keepdim=True)
 kernel_fn = lambda x, y: laplacian(x, y, bandwidth=1.)
 #kernel_fn = lambda x, z: relu_ntk(x, z, depth=3)
 
-model = eigenpro.EigenProRegressor(kernel_fn, x_train, n_class, device=DEVICE)
+model = eigenpro.KernelModel(kernel_fn, x_train, n_class, device=DEVICE)
 
 results = model.fit(x_train, y_train, x_test, y_test, epochs=20, print_every=2, mem_gb=20)
