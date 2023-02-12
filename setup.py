fcb20e2a6acb9f21b64c435fc702288a526200ea
Parthe Pandit <parthe@users.noreply.github.com>
2023-02-12 08:56:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='eigenpro2',
     version='2.0.0',
     author='Siyuan Ma, Adityanarayanan Radhakrishnan, Parthe Pandit',
